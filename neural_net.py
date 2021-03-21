24a7edb70a9a1c47a41e8ce6aae6be2cbe97bfd5
fehim <fehimornek@gmail.com>
2021-03-21 16:17:58 +0300

    added bunch of optimization algorithms suchs as RMSprop, GD with momentum and most importantly ADAM. Loss is still about 0.2

--- neural_net.py
+++ neural_net.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 #binary classification using a modular deep neural network
 
 def loadData():
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
     trainData = pd.read_csv("titanic/train.csv")
     testData = pd.read_csv("titanic/test.csv")
     testData2 = pd.read_csv("titanic/gender_submission.csv")
@@ -42,7 +44,7 @@ def loadData():
     dict = {"female": 0, "male": 1}
     X_train = X_train.replace({"Sex":dict})
     X_test = X_test.replace({"Sex":dict})
-    return np.array(X_train), np.array(Y_train), np.array(X_test), np.array(Y_test)
+    return np.array(X_train).T, np.array(Y_train).T, np.array(X_test).T, np.array(Y_test).T
 
 def xaiverInit(layer_dims,activation_func):
     parameters = {}
@@ -71,12 +73,26 @@ def reLU(Z):
 def reLU_prime(Z):
     return np.where(Z > 0, 1.0, 0.0)
 
+
 def compute_cost(A_last,Y):
     m = len(Y.T)
     cross_ent_cost = -(1/m) * np.sum(Y * np.log(A_last) + (1-Y) * np.log(1 - A_last))
     return cross_ent_cost
 
-def forward_prop(X,parameters,activation_funcs):
+def predict(X_test,Y_test,parameters,activation_funcs):
+    cache = forward_prop(X_test,parameters,activation_funcs)
+    prediction = cache["A" + str(len(cache)//2)]
+    prediction.flatten()
+    for i in range(len(prediction)):
+        if prediction[0][i] >= 0.5:
+            prediction[0][i] = 1
+        else:
+            prediction[0][i] = 0
+    error = abs(prediction - Y_test)
+    error = error[~np.isnan(error)]
+    return np.sum(error) /len(error)
+
+def forward_prop(X, parameters, activation_funcs):
     cache = {"A0": X}
     for i in range(1, (len(parameters)//2) + 1):
         cache["Z" + str(i)] = np.dot(parameters["W" + str(i)], cache["A" + str(i-1)]) + parameters["b" + str(i)]
@@ -89,20 +105,12 @@ def forward_prop(X,parameters,activation_funcs):
 def calc_gradient(cache,parameters,activation_funcs,Y):
     m = len(Y)
     grads = {}
-    index_of_A_last = len(parameters) // 2
-    dA_last = -(Y/cache["A" + str(index_of_A_last)]) + ((1- Y)/1-cache["A" + str(index_of_A_last)])
-
-    for i in range(len(parameters)//2, 0, -1):
-        if activation_funcs[i-1] == "sigmoid":
-            if i == len(parameters) // 2:
-                grads["dZ" + str(i)] = dA_last * sigmoid_prime(cache["Z" + str(i)])
-                grads["dW" + str(i)] = (1/m) * np.dot(grads["dZ" + str(i)],cache["A" + str(i-1)].T)
-                grads["db" + str(i)] = (1/m) * np.sum(grads["dZ" + str(i)],axis=1,keepdims=True)
-            else:
-                grads["dZ" + str(i)] = np.dot(parameters["W" + str(i+1)].T, grads["dZ" + str(i+1)]) * sigmoid_prime(cache["Z" + str(i)])
-                grads["dW" + str(i)] = (1/m) * np.dot(grads["dZ" + str(i)], cache["A" + str(i-1)].T)
-                grads["db" + str(i)] = (1/m) * np.sum(grads["dZ" + str(i)],axis=1,keepdims=True)
-        elif activation_funcs[i-1] == "relu":
+    index_of_last = len(parameters) // 2
+    dA_last = -(Y/cache["A" + str(index_of_last)]) + ((1- Y)/(1-cache["A" + str(index_of_last)]))
+    grads["dZ" + str(index_of_last)] = dA_last * sigmoid_prime(cache["Z" + str(index_of_last)])
+    grads["dW" + str(index_of_last)] = (1/m) * np.dot(grads["dZ" + str(index_of_last)], cache["A" + str(index_of_last - 1)].T)
+    grads["db" + str(index_of_last)] = (1/m) * np.sum(grads["dZ" + str(index_of_last)], axis=1, keepdims=True)
+    for i in range((len(parameters)//2)-1, 0, -1):
             grads["dZ" + str(i)] = np.dot(parameters["W" + str(i+1)].T, grads["dZ" + str(i+1)]) * reLU_prime(cache["Z" + str(i)])
             grads["dW" + str(i)] = (1/m) * np.dot(grads["dZ" + str(i)], cache["A" + str(i-1)].T)
             grads["db" + str(i)] = (1/m) * np.sum(grads["dZ" + str(i)],axis=1,keepdims=True)
@@ -114,16 +122,79 @@ def back_prop(grads,parameters,learning_rate):
         parameters["b" + str(i)] = parameters["b" + str(i)] - learning_rate * grads["db" + str(i)]
     return parameters
 
+def initialize_velocity(layer_dims):
+    V = {}
+    for i in range(1, len(layer_dims)):
+        V["Vdw" + str(i)] = np.zeros([layer_dims[i],layer_dims[i-1]])
+        V["Vdb" + str(i)] = np.zeros([layer_dims[i],1])
+    return V
+
+def back_prop_momentum(velocities, grads,parameters,learning_rate,beta = 0.9):
+    for i in range(1, (len(parameters) // 2) + 1):
+        velocities["Vdw" + str(i)] = beta * velocities["Vdw" + str(i)] + (1 - beta) * grads["dW" + str(i)]
+        velocities["Vdb" + str(i)] = beta * velocities["Vdb" + str(i)] + (1 - beta) * grads["db" + str(i)]
+        parameters["W" + str(i)] = parameters["W" + str(i)] - learning_rate * velocities["Vdw" + str(i)]
+        parameters["b" + str(i)] = parameters["b" + str(i)] - learning_rate * velocities["Vdb" + str(i)]
+    return parameters, velocities
+
+
+def initialize_rms(layer_dims):
+    S = {}
+    for i in range(1, len(layer_dims)):
+        S["Sdw" + str(i)] = np.zeros([layer_dims[i],layer_dims[i-1]])
+        S["Sdb" + str(i)] = np.zeros([layer_dims[i],1])
+    return S
+
+
+def RMSprop(s_val, velocities, grads,parameters,learning_rate,beta = 0.999,epsilon=10e-8):
+    for i in range(1, (len(parameters) // 2) + 1):
+        s_val["Sdw" + str(i)] = beta * s_val["Sdw" + str(i)] + (1 - beta) * (grads["dW" + str(i)]**2)
+        s_val["Sdb" + str(i)] = beta * s_val["Sdb" + str(i)] + (1 - beta) * (grads["db" + str(i)]**2)
+        parameters["W" + str(i)] = parameters["W" + str(i)] - learning_rate * (grads["dW" + str(i)] / np.sqrt(s_val["Sdw" + str(i)]+epsilon))
+        parameters["b" + str(i)] = parameters["b" + str(i)] - learning_rate * (grads["db" + str(i)] / np.sqrt(s_val["Sdb" + str(i)]+epsilon))
+    return parameters, s_val
+
+
+def ADAM_optimizer(velocities, s_val, grads,parameters,learning_rate,iteration,beta1 = 0.9, beta2 = 0.999, epsilon=10e-8):
+    velocities_corrected = {}
+    s_corrected = {}
+    for i in range(1, (len(parameters) // 2) + 1):
+        velocities["Vdw" + str(i)] = beta1 * velocities["Vdw" + str(i)] + (1 - beta1) * grads["dW" + str(i)]
+        velocities["Vdb" + str(i)] = beta1 * velocities["Vdb" + str(i)] + (1 - beta1) * grads["db" + str(i)]
+        s_val["Sdw" + str(i)] = beta2 * s_val["Sdw" + str(i)] + (1 - beta2) * (grads["dW" + str(i)]**2)
+        s_val["Sdb" + str(i)] = beta2 * s_val["Sdb" + str(i)] + (1 - beta2) * (grads["db" + str(i)]**2)
+        velocities_corrected["Vdw" + str(i)] = velocities["Vdw" + str(i)] / (1 - beta1** iteration)
+        velocities_corrected["Vdb" + str(i)] = velocities["Vdb" + str(i)] / (1 - beta1 ** iteration)
+        s_corrected["Sdw" + str(i)] = s_val["Sdw" + str(i)]  / (1 - beta2 ** iteration)
+        s_corrected["Sdb" + str(i)] = s_val["Sdb" + str(i)] / (1 - beta2 ** iteration)
+        parameters["W" + str(i)] -= learning_rate * (velocities_corrected["Vdw" + str(i)] / np.sqrt(s_corrected["Sdw" + str(i)]+epsilon))
+        parameters["b" + str(i)] -= learning_rate * (velocities_corrected["Vdb" + str(i)] / np.sqrt(s_corrected["Sdb" + str(i)]+epsilon))
+    return parameters, s_val, velocities
+
 
 def NeuralNet(layer_dims,activation_funcs,iterations,learning_rate):
     X_train, Y_train, X_test, Y_test = loadData()
     parameters = xaiverInit(layer_dims, activation_funcs)
-    X_train = X_train.T
-    Y_train = Y_train.T
-    for i in range(1,5):
+    Velocities = initialize_velocity(layer_dims)            # initialize V's as zeros
+    S = initialize_rms(layer_dims)
+    cost_list = []
+    for i in range(1,iterations):
         cache = forward_prop(X_train, parameters,activation_funcs)
-        cost = compute_cost(cache["A3"],Y_train)
+        cost = compute_cost(cache["A"+str(len(cache)//2)], Y_train)
         grads = calc_gradient(cache,parameters,activation_funcs,Y_train)
-        parameters = back_prop(grads,parameters,learning_rate)
+        #parameters = back_prop(grads,parameters,learning_rate)
+        #parameters,Velocities = back_prop_momentum(Velocities,grads,parameters,learning_rate)
+        #parameters, S = RMSprop(S,grads,parameters,learning_rate)
+        parameters,S, Velocities = ADAM_optimizer(Velocities,S,grads,parameters,learning_rate,i)
+        cost_list.append(cost)
         print(cost)
-NeuralNet([4, 5, 3, 1], ["relu", "relu", "sigmoid"],iterations=11000,learning_rate=0.0075)
+    hata = predict(X_test,Y_test,parameters,activation_funcs)
+    print(hata)
+    return cost_list
+
+cost_list = NeuralNet([4,20,20,10, 1], ["relu", "relu","relu", "sigmoid"],iterations=6000, learning_rate=0.0003)
+X = []
+for i in range(len(cost_list)):
+    X.append(i)
+plt.plot(X,cost_list)
+plt.show()
